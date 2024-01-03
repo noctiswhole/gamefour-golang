89537589c6d59f4fe822371ad65ceee34f1ebbaa
noctiswhole <anthony@creates.tech>
2024-01-03 14:12:22 -0600

    Move GLFW and GL calls into Game struct methods

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module gamefour-golang
 go 1.21
 
 require (
-	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
-	github.com/go-gl/glfw v0.0.0-20231223183121-56fa3ac82ce7 // indirect
-	github.com/go-gl/mathgl v1.1.0 // indirect
-	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71
+	github.com/go-gl/glfw v0.0.0-20231223183121-56fa3ac82ce7
 )
--- main.go
+++ main.go
@@ -1,48 +1,88 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-gl/gl/v4.6-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"os"
+	"runtime"
 )
 
-func resizeCallback(w *glfw.Window, width int, height int) {
-	gl.Viewport(0, 0, int32(width), int32(height))
+type Game struct {
+	window *glfw.Window
 }
 
-func processInput(w *glfw.Window) {
-	if w.GetKey(glfw.KeyEscape) == glfw.Press {
-		w.SetShouldClose(true)
-	}
-}
-
-func main() {
-	_ = glfw.Init()
-	defer glfw.Terminate()
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 6)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-
-	window, err := glfw.CreateWindow(1920, 1080, "hello golang!", nil, nil)
-	if err != nil {
-		os.Exit(-1)
-	}
-
-	window.MakeContextCurrent()
-
+func (g Game) InitGL() error {
 	if err := gl.Init(); err != nil {
-		os.Exit(-1)
+		return err
 	}
 
 	gl.Viewport(0, 0, 1920, 1080)
-	window.SetFramebufferSizeCallback(resizeCallback)
+	return gl.Init()
+}
 
-	for !window.ShouldClose() {
-		processInput(window)
-		gl.ClearColor(1, 0, 1, 1)
-		gl.Clear(gl.COLOR_BUFFER_BIT)
-		window.SwapBuffers()
-		glfw.PollEvents()
+func (g *Game) Init() error {
+	runtime.LockOSThread()
+	if err := glfw.Init(); err != nil {
+		return err
+	}
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 6)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	window, err := glfw.CreateWindow(1920, 1080, "hello golang!", nil, nil)
+	if err != nil {
+		return err
+	}
+
+	window.MakeContextCurrent()
+	err = g.InitGL()
+	window.SetFramebufferSizeCallback(
+		func(w *glfw.Window, width int, height int) { gl.Viewport(0, 0, int32(width), int32(height)) })
+
+	g.window = window
+
+	return err
+}
+
+func (g Game) Destroy() {
+	glfw.Terminate()
+}
+
+func (g Game) ProcessInput() {
+	glfw.PollEvents()
+	if g.window.GetKey(glfw.KeyEscape) == glfw.Press {
+		g.window.SetShouldClose(true)
+	}
+}
+
+func (g Game) ClearBuffer() {
+	gl.ClearColor(1, 0, 1, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+}
+
+func (g Game) SwapBuffer() {
+	g.window.SwapBuffers()
+}
+
+func (g Game) ShouldClose() bool {
+	return g.window.ShouldClose()
+}
+
+func main() {
+	game := Game{}
+
+	if err := game.Init(); err != nil {
+		fmt.Printf("%s", err.Error())
+		os.Exit(-1)
+	}
+
+	defer game.Destroy()
+
+	for !game.ShouldClose() {
+		game.ProcessInput()
+		game.ClearBuffer()
+		game.SwapBuffer()
 	}
 
 }
